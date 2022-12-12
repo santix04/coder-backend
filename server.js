ba8086179ef5ae4e06a423c764e7f399c8d60dc4
santix <santiagohelguera04@gmail.com>
2022-12-12 16:44:43 -0300

    desafio-4

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const express = require('express');
-const Container = require('./contenedor');
+const express = require("express");
+const bp = require("body-parser");
+const router = require("./routers/index");
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use("/api", router);
 
 const server = app.listen(PORT, () => {
-  console.log(`Listening app port ${server.address().port}`);
+    console.log(
+        `Servidor en el puerto ${server.address().port}`
+    );
+    console.log(`http://localhost:${server.address().port}`);
 });
-
-
-server.on('error', (error) => {
-  console.log('Error', error);
-});
-
-const products = new Container('./productos.json')
-
-app.get('/productos', async (req, res) => {
-  const productos = await products.getAll();
-  res.send(productos);
-});
-
-
-app.get('/productoRandom', async (req, res) => {
-  const productos = await products.getAll();
-  let numeroRandom = Math.floor(Math.random() * productos.length);
-  res.send(productos[numeroRandom]);
-});
+server.on("error", error => console.log(`Error en servidor: ${error}`));
